edc58ba71688aca8e7e2134b163daeec73d1eae4
Saud12131 <saudsayyedofficial@gmail.com>
2025-11-01 15:00:37 +0530

    fix - [BUG] : Profile pictures

--- apps/web/src/components/dashboard/ProfilePic.tsx
+++ apps/web/src/components/dashboard/ProfilePic.tsx
@@ -2,13 +2,14 @@
 
 import Image from "next/image";
 import imgUrl from "../../assets/images/user_dp.png";
-
+import { useSession } from "next-auth/react";
 export const ProfilePic = () => {
+    const { data: session } = useSession();
   return (
     <div className="rounded-full overflow-hidden inline-block h-8 w-8 border">
       <Image
         alt="User Profile"
-        src={imgUrl}
+        src={session?.user?.image||imgUrl}
         className="w-full h-full object-cover"
         width={10}
         height={10}
